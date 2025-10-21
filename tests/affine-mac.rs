eeb3388b77241b3e3ea6f3bc55bc3ca275bc468c
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2025-10-21 14:14:58 +0200

    Restructure unit tests for affine MAC

--- tests/affine-mac.rs
+++ tests/affine-mac.rs
@@ -1,9 +1,9 @@
 use ibe_schemes::*;
 
 #[test]
-fn test_affine_mac_2() {
-    let k = 2usize;
-    let m_len = 4usize;
+fn affine_mac_small_ok() {
+    let k = 2;
+    let m_len = 4;
     let l = 2 * m_len + 1;
     let l_prime = 0;
     let mac = AffineMAC::new(k, l, l_prime);
@@ -12,43 +12,48 @@ fn test_affine_mac_2() {
     let message = vec![1u8, 0, 1, 1];
     let tag = mac.tag(&sk, &message);
     let check = mac.verify(&sk, &message, &tag);
-    assert!(check, "valid tag should verify");
-
-    let new_message = vec![0u8, 0, 1, 1];
-    let check2 = mac.verify(&sk, &new_message, &tag);
-    assert!(!check2, "tag for different message should not verify");
+    assert!(check);
 }
 
 #[test]
-fn test_affine_mac() {
+fn affine_mac_small_fail() {
+    let k = 2;
+    let m_len = 4;
+    let l = 2 * m_len + 1;
+    let l_prime = 0;
+    let mac = AffineMAC::new(k, l, l_prime);
+    let sk = mac.gen_mac();
+
+    let message = vec![1u8, 0, 1, 1];
+    let tag = mac.tag(&sk, &message);
+    let new_message = vec![0u8, 0, 1, 1];
+    let check = mac.verify(&sk, &new_message, &tag);
+    assert!(!check);
+}
+
+#[test]
+fn affine_mac_large_ok() {
     let m_len = 128;
     let l = 2 * m_len + 1;
     let mac = AffineMAC::new(2, l, 0);
-
     let sk = mac.gen_mac();
 
     let message = generate_random_message_128();
-    println!("Random Message: {:?}", &message);
-    let message = generate_random_message_128();
-    println!(
-        "Message length: {} bytes = {} bits",
-        message.len(),
-        message.len() * 8
-    );
-
     let tag = mac.tag(&sk, &message);
-    let verified = mac.verify(&sk, &message, &tag);
+    let check = mac.verify(&sk, &message, &tag);
+    assert!(check);
+}
 
-    println!(
-        "\nMAC verification: {}",
-        if verified { "Success" } else { "Failed" }
-    );
+#[test]
+fn affine_mac_large_fail() {
+    let m_len = 128;
+    let l = 2 * m_len + 1;
+    let mac = AffineMAC::new(2, l, 0);
+    let sk = mac.gen_mac();
 
-    // Test with wrong message
-    let wrong_message = generate_random_message_128();
-    let wrong_verified = mac.verify(&sk, &wrong_message, &tag);
-    println!(
-        "Wrong message verification: {}",
-        if !wrong_verified { "Failed" } else { "Success" }
-    );
+    let message = generate_random_message_128();
+    let tag = mac.tag(&sk, &message);
+    let new_message = generate_random_message_128();
+    let check = mac.verify(&sk, &new_message, &tag);
+    assert!(!check);
 }
