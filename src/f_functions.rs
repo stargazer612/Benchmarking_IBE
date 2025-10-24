5c39ef5a6b41536cccee7680f43b4cbf5895066b
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2025-10-24 15:15:39 +0200

    Simplify logic of f_i

--- src/f_functions.rs
+++ src/f_functions.rs
@@ -1,26 +1,25 @@
+use bit_vec::BitVec;
+
 pub fn f_i(i: usize, l: usize, message: &[u8]) -> u8 {
-    match i {
-        0 | 1 => 0,
-        _ => {
-            let bit_index = (i - 2) / 2;
-            let bit_value = (i - 2) % 2;
-
-            if bit_index < l && bit_index < message.len() * 8 {
-                let byte_index = bit_index / 8;
-                let bit_position = bit_index % 8;
-
-                if byte_index < message.len() {
-                    let message_bit = ((message[byte_index] >> bit_position) & 1) as usize;
-
-                    if message_bit == bit_value { 1 } else { 0 }
-                } else {
-                    0
-                }
-            } else {
-                0
-            }
-        }
+    if i < 2 {
+        return 0;
     }
+
+    let bit_index = (i - 2) / 2;
+    let bit_value = (i - 2) % 2;
+
+    if bit_index >= l || bit_index >= message.len() * 8 {
+        return 0;
+    }
+
+    let msg = BitVec::from_bytes(message);
+    // Original code accesses bits of each byte from LSB to MSB
+    // BitVec access bits of each byte from MSB to LSB
+    // This idx maps between both to keep original behavior
+    let idx = (bit_index / 8) * 8 + (7 - bit_index % 8);
+    let message_bit = msg[idx] as usize;
+
+    if message_bit == bit_value { 1 } else { 0 }
 }
 
 pub fn f_prime_i(i: usize) -> u8 {
