1017e99d6c5a6a0f242a8fdf351f2242ce198271
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2025-10-24 14:07:18 +0200

    Add unit tests for f_i and f_prime_i functions

---
+++ tests/f-functions.rs
@@ -0,0 +1,66 @@
+use ibe_schemes::f_functions::*;
+use ibe_schemes::types::*;
+
+use ark_ff::{One, Zero};
+
+#[test]
+fn test_f_0() {
+    let l = 128;
+    let msg = [];
+    assert_eq!(f_i(0, l, &msg), FieldElement::zero());
+}
+
+#[test]
+fn test_f_1() {
+    let l = 128;
+    let msg = [];
+    assert_eq!(f_i(1, l, &msg), FieldElement::zero());
+}
+
+#[test]
+fn test_f_i_larger_than_l() {
+    let l = 128;
+    let msg = [];
+    // (258 - 2)/2 = 128 >= l
+    assert_eq!(f_i(258, l, &msg), FieldElement::zero());
+}
+
+#[test]
+fn test_f_i_larger_than_msg() {
+    let l = 128;
+    let msg = [0, 1, 2, 3, 4, 5];
+    // (128 - 2)/2 = 63 < l, but 63 > msg.len()*8
+    assert_eq!(f_i(128, l, &msg), FieldElement::zero());
+}
+
+#[test]
+fn test_f_n() {
+    let l = 128;
+    let msg = [
+        8, 150, 201, 77, 104, 204, 30, 51, 189, 255, 234, 17, 48, 111, 0, 1,
+    ];
+    assert_eq!(f_i(6, l, &msg), FieldElement::one());
+    assert_eq!(f_i(35, l, &msg), FieldElement::one());
+    assert_eq!(f_i(100, l, &msg), FieldElement::zero());
+    assert_eq!(f_i(159, l, &msg), FieldElement::one());
+    assert_eq!(f_i(201, l, &msg), FieldElement::zero());
+    assert_eq!(f_i(257, l, &msg), FieldElement::zero());
+}
+
+#[test]
+fn test_f_prime_0() {
+    assert_eq!(f_prime_i(0), FieldElement::one());
+}
+
+#[test]
+fn test_f_prime_1() {
+    assert_eq!(f_prime_i(1), FieldElement::zero());
+}
+
+#[test]
+fn test_f_prime_n() {
+    assert_eq!(f_prime_i(2), FieldElement::zero());
+    assert_eq!(f_prime_i(5), FieldElement::zero());
+    assert_eq!(f_prime_i(94), FieldElement::zero());
+    assert_eq!(f_prime_i(1354154), FieldElement::zero());
+}
