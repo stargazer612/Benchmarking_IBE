f3cf8e2d6b6fb24bc35db33e5cd32963a7351139
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2025-10-28 11:27:05 +0100

    Draft keygen function for LW-HIBE

--- src/lw.rs
+++ src/lw.rs
@@ -21,6 +21,13 @@ pub struct MPK {
     pub b_1_g2: G2,
 }
 
+pub struct USK {
+    pub identity: Vec<String>,
+    pub k: Vec<G2>,
+    pub k_1: Vec<G2>,
+    pub k_2: Vec<G2>,
+}
+
 pub struct LW {}
 
 impl LW {
@@ -53,5 +60,49 @@ impl LW {
         };
 
         (msk, mpk)
+    }
+
+    pub fn keygen(&self, mut rng: impl Rng, msk: &MSK, identity: Vec<String>) -> USK {
+        let n_k = identity.len();
+        assert!(n_k > 0);
+
+        let g2 = G2::generator();
+
+        let mut k = Vec::with_capacity(n_k);
+        let mut k_1 = Vec::with_capacity(n_k);
+        let mut k_2 = Vec::with_capacity(n_k);
+
+        let mut rs = Vec::with_capacity(n_k);
+        for _ in 0..n_k {
+            let r_i = Fr::rand(&mut rng);
+            rs.push(r_i);
+            k.push(g2 * r_i);
+        }
+
+        let mut lambdas = Vec::with_capacity(n_k);
+        let mut sum = Fr::zero();
+        lambdas.push(Fr::zero());
+        for _ in 1..n_k {
+            let lambda_i = Fr::rand(&mut rng);
+            sum += lambda_i;
+        }
+        lambdas[0] = msk.alpha - sum;
+
+        for i in 0..n_k {
+            let e_1 = lambdas[i] + rs[i] * msk.b;
+            k_1.push(g2 * e_1);
+
+            // TODO: properly hash id_i to G2
+            let xid = Fr::rand(&mut rng);
+            let e_2 = rs[i] * (msk.b_0 + xid * msk.b_1);
+            k_2.push(g2 * e_2);
+        }
+
+        USK {
+            identity: identity.clone(),
+            k,
+            k_1,
+            k_2,
+        }
     }
 }
