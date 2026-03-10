b72459d20f2bf3ad6499f30b556f193a75f838b5
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2026-03-10 13:51:47 +0100

    Rewrite benchmarks to use fresh values in each iteration

--- benches/bb.rs
+++ benches/bb.rs
@@ -1,13 +1,27 @@
 use ark_bls12_381::Fq12 as Gt;
 use ark_ff::UniformRand;
 
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use std::time::Instant;
 
 use std::hint::black_box as blb;
 
 use ibe_schemes::pes::{IBEScheme, bb::*};
 
-use rand::thread_rng;
+use rand::{Rng, thread_rng};
+
+// Performance of BB scheme should be independent of identity length
+// const SIZES: [usize; 9] = [1, 2, 5, 10, 15, 20, 50, 100, 250];
+const SIZES: [usize; 1] = [5];
+
+fn rand_string(len: usize) -> String {
+    let mut rng = thread_rng();
+    let chars = b"abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    (0..len)
+        .map(|_| chars[rng.gen_range(0..chars.len())] as char)
+        .collect()
+}
 
 pub fn bench_bb_new(c: &mut Criterion) {
     c.bench_function("bb_new", |b| b.iter(|| BB::new()));
@@ -22,38 +36,93 @@ pub fn bench_bb_setup(c: &mut Criterion) {
 
 pub fn bench_bb_keygen(c: &mut Criterion) {
     let mut rng = thread_rng();
-    let bb = BB::new();
-    let (msk, _) = bb.setup(&mut rng);
-    let identity = String::from("ABCDEF");
 
-    c.bench_function("bb_keygen", |b| {
-        b.iter(|| bb.keygen(blb(&mut rng), blb(&msk), blb(identity.clone())))
-    });
+    let mut group = c.benchmark_group("bb_keygen");
+    for size in SIZES.iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter_custom(|n| {
+                let bb = BB::new();
+                let (msk, _) = bb.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_string(size)).collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = ids
+                    .into_iter()
+                    .map(|id| bb.keygen(&mut rng, &msk, id))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
 }
 
 pub fn bench_bb_encrypt(c: &mut Criterion) {
     let mut rng = thread_rng();
-    let bb = BB::new();
-    let (_, mpk) = bb.setup(&mut rng);
-    let identity = String::from("ABCDEF");
-    let k = Gt::rand(&mut rng);
 
-    c.bench_function("bb_encrypt", |b| {
-        b.iter(|| bb.encrypt(blb(&mut rng), blb(&k), blb(&mpk), blb(identity.clone())))
-    });
+    let mut group = c.benchmark_group("bb_encrypt");
+    for size in SIZES.iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter_custom(|n| {
+                let bb = BB::new();
+                let (_, mpk) = bb.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_string(size)).collect::<Vec<_>>();
+                let ks = (0..n).map(|_| Gt::rand(&mut rng)).collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = ids
+                    .into_iter()
+                    .zip(ks)
+                    .map(|(id, k)| bb.encrypt(&mut rng, &k, &mpk, id))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
 }
 
 pub fn bench_bb_decrypt(c: &mut Criterion) {
     let mut rng = thread_rng();
-    let bb = BB::new();
-    let (msk, mpk) = bb.setup(&mut rng);
-    let identity = String::from("ABCDEF");
-    let usk = bb.keygen(&mut rng, &msk, identity.clone());
 
-    let k = Gt::rand(&mut rng);
-    let ct = bb.encrypt(&mut rng, &k, &mpk, identity);
+    let mut group = c.benchmark_group("bb_decrypt");
+    for size in SIZES.iter() {
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter_custom(|n| {
+                let bb = BB::new();
+                let (msk, mpk) = bb.setup(&mut rng);
 
-    c.bench_function("bb_decrypt", |b| b.iter(|| bb.decrypt(blb(&usk), blb(&ct))));
+                let ids = (0..n).map(|_| rand_string(size)).collect::<Vec<_>>();
+                let usks = ids
+                    .iter()
+                    .map(|i| bb.keygen(&mut rng, &msk, i.clone()))
+                    .collect::<Vec<_>>();
+                let ks = ids.iter().map(|_| Gt::rand(&mut rng)).collect::<Vec<_>>();
+                let cts = ids
+                    .iter()
+                    .zip(ks)
+                    .map(|(i, k)| bb.encrypt(&mut rng, &k, &mpk, i.clone()))
+                    .collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = usks
+                    .iter()
+                    .zip(cts)
+                    .map(|(usk, ct)| bb.decrypt(&usk, &ct))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(
