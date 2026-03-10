2f1e166e4590de0ac7e1b0010c01a470ae2c3962
Sven Argo <sven.argo@ruhr-uni-bochum.de>
2026-03-10 15:50:34 +0100

    Add benchmark target for LW scheme and extract common patterns

--- benches/common.rs
+++ benches/common.rs
@@ -8,7 +8,7 @@ use std::time::Instant;
 
 use std::hint::black_box as blb;
 
-use ibe_schemes::pes::IBEScheme;
+use ibe_schemes::pes::{HIBEScheme, IBEScheme};
 
 fn rand_string(len: usize) -> String {
     let mut rng = thread_rng();
@@ -17,6 +17,14 @@ fn rand_string(len: usize) -> String {
     (0..len)
         .map(|_| chars[rng.gen_range(0..chars.len())] as char)
         .collect()
+}
+
+fn rand_hierarchy(depth: usize, id_len: usize) -> Vec<String> {
+    (0..depth).map(|_| rand_string(id_len)).collect()
+}
+
+fn parse_identity(id: &str) -> Vec<String> {
+    id.split(".").map(|s| String::from(s)).collect()
 }
 
 pub fn bench_ibe_scheme_setup<T: IBEScheme>(scheme: T, c: &mut Criterion) {
@@ -106,6 +114,155 @@ pub fn bench_ibe_scheme_decrypt<T: IBEScheme>(scheme: T, sizes: &[usize], c: &mu
                     .iter()
                     .zip(cts)
                     .map(|(usk, ct)| scheme.decrypt(&usk, &ct))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn bench_hibe_scheme_setup<T: HIBEScheme>(scheme: T, c: &mut Criterion) {
+    let desc = format!("{}_setup", scheme.name());
+    let mut rng = thread_rng();
+    c.bench_function(&desc, |b| b.iter(|| scheme.setup(&mut rng)));
+}
+
+pub fn bench_hibe_scheme_keygen<T: HIBEScheme>(
+    scheme: T,
+    depths: &[usize],
+    size: usize,
+    c: &mut Criterion,
+) {
+    let mut rng = thread_rng();
+
+    let desc = format!("{}_keygen", scheme.name());
+    let mut group = c.benchmark_group(&desc);
+    for d in depths {
+        group.bench_with_input(BenchmarkId::from_parameter(d), d, |b, &d| {
+            b.iter_custom(|n| {
+                let (msk, _) = scheme.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_hierarchy(d, size)).collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = ids
+                    .into_iter()
+                    .map(|id| scheme.keygen(&mut rng, &msk, id))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn bench_hibe_scheme_encrypt<T: HIBEScheme>(
+    scheme: T,
+    depths: &[usize],
+    size: usize,
+    c: &mut Criterion,
+) {
+    let mut rng = thread_rng();
+
+    let desc = format!("{}_encrypt", scheme.name());
+    let mut group = c.benchmark_group(&desc);
+    for d in depths {
+        group.bench_with_input(BenchmarkId::from_parameter(d), d, |b, &d| {
+            b.iter_custom(|n| {
+                let (_, mpk) = scheme.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_hierarchy(d, size)).collect::<Vec<_>>();
+                let ks = (0..n).map(|_| Gt::rand(&mut rng)).collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = ids
+                    .into_iter()
+                    .zip(ks)
+                    .map(|(id, k)| scheme.encrypt(&mut rng, &k, &mpk, id))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn bench_hibe_scheme_decrypt<T: HIBEScheme>(
+    scheme: T,
+    depths: &[usize],
+    size: usize,
+    c: &mut Criterion,
+) {
+    let mut rng = thread_rng();
+
+    let desc = format!("{}_decrypt", scheme.name());
+    let mut group = c.benchmark_group(&desc);
+    for d in depths {
+        group.bench_with_input(BenchmarkId::from_parameter(d), d, |b, &d| {
+            b.iter_custom(|n| {
+                let (msk, mpk) = scheme.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_hierarchy(d, size)).collect::<Vec<_>>();
+                let usks = ids
+                    .iter()
+                    .map(|i| scheme.keygen(&mut rng, &msk, i.clone()))
+                    .collect::<Vec<_>>();
+                let ks = ids.iter().map(|_| Gt::rand(&mut rng)).collect::<Vec<_>>();
+                let cts = ids
+                    .iter()
+                    .zip(ks)
+                    .map(|(i, k)| scheme.encrypt(&mut rng, &k, &mpk, i.clone()))
+                    .collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = usks
+                    .iter()
+                    .zip(cts)
+                    .map(|(usk, ct)| scheme.decrypt(&usk, &ct))
+                    .collect::<Vec<_>>();
+                let time = start.elapsed();
+                let _ = blb(res);
+                time
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn bench_hibe_scheme_delegate<T: HIBEScheme>(
+    scheme: T,
+    depths: &[usize],
+    size: usize,
+    c: &mut Criterion,
+) {
+    let mut rng = thread_rng();
+
+    let desc = format!("{}_delegate", scheme.name());
+    let mut group = c.benchmark_group(&desc);
+    for d in depths {
+        group.bench_with_input(BenchmarkId::from_parameter(d), d, |b, &d| {
+            b.iter_custom(|n| {
+                let (msk, mpk) = scheme.setup(&mut rng);
+
+                let ids = (0..n).map(|_| rand_hierarchy(d, size)).collect::<Vec<_>>();
+                let usks = ids
+                    .iter()
+                    .map(|i| scheme.keygen(&mut rng, &msk, i.clone()))
+                    .collect::<Vec<_>>();
+                let extensions = (0..n).map(|_| rand_string(size)).collect::<Vec<_>>();
+
+                let start = Instant::now();
+                let res = usks
+                    .iter()
+                    .zip(extensions)
+                    .map(|(usk, ext)| scheme.delegate(&mut rng, &mpk, &usk, ext))
                     .collect::<Vec<_>>();
                 let time = start.elapsed();
                 let _ = blb(res);
--- benches/lw.rs
+++ benches/lw.rs
@@ -1,82 +1,35 @@
-use ark_bls12_381::Fq12 as Gt;
-use ark_ff::UniformRand;
-
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use std::hint::black_box as bb;
+use ibe_schemes::pes::lw::*;
 
-use ibe_schemes::pes::{HIBEScheme, lw::*};
+mod common;
+use common::*;
 
-use rand::thread_rng;
-
-fn parse_identity(id: &str) -> Vec<String> {
-    id.split(".").map(|s| String::from(s)).collect()
-}
-
-pub fn bench_lw_new(c: &mut Criterion) {
-    c.bench_function("lw_new", |b| b.iter(|| LW::new()));
-}
+const DEPTHS: [usize; 9] = [1, 2, 5, 10, 15, 20, 50, 100, 250];
+const ID_SIZE: usize = 5;
 
 pub fn bench_lw_setup(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let lw = LW::new();
-
-    c.bench_function("lw_setup", |b| b.iter(|| lw.setup(&mut rng)));
+    bench_hibe_scheme_setup(LW::new(), c);
 }
 
 pub fn bench_lw_keygen(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let lw = LW::new();
-    let (msk, _) = lw.setup(&mut rng);
-    let identity = parse_identity("A.B.C.D");
-
-    c.bench_function("lw_keygen", |b| {
-        b.iter(|| lw.keygen(bb(&mut rng), bb(&msk), bb(identity.clone())))
-    });
+    bench_hibe_scheme_keygen(LW::new(), &DEPTHS, ID_SIZE, c);
 }
 
 pub fn bench_lw_encrypt(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let lw = LW::new();
-    let (_, mpk) = lw.setup(&mut rng);
-    let identity = parse_identity("A.B.C.D");
-    let k = Gt::rand(&mut rng);
-
-    c.bench_function("lw_encrypt", |b| {
-        b.iter(|| lw.encrypt(bb(&mut rng), bb(&k), bb(&mpk), bb(identity.clone())))
-    });
+    bench_hibe_scheme_encrypt(LW::new(), &DEPTHS, ID_SIZE, c);
 }
 
 pub fn bench_lw_decrypt(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let lw = LW::new();
-    let (msk, mpk) = lw.setup(&mut rng);
-    let identity = parse_identity("A.B.C.D");
-    let usk = lw.keygen(&mut rng, &msk, identity.clone());
-
-    let k = Gt::rand(&mut rng);
-    let ct = lw.encrypt(&mut rng, &k, &mpk, identity);
-
-    c.bench_function("lw_decrypt", |b| b.iter(|| lw.decrypt(bb(&usk), bb(&ct))));
+    bench_hibe_scheme_decrypt(LW::new(), &DEPTHS, ID_SIZE, c);
 }
 
 pub fn bench_lw_delegate(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let lw = LW::new();
-    let (msk, mpk) = lw.setup(&mut rng);
-    let identity = parse_identity("A.B.C.D");
-    let usk = lw.keygen(&mut rng, &msk, identity.clone());
-
-    let extension = String::from("E");
-
-    c.bench_function("lw_delegate", |b| {
-        b.iter(|| lw.delegate(bb(&mut rng), bb(&mpk), bb(&usk), bb(extension.clone())))
-    });
+    bench_hibe_scheme_delegate(LW::new(), &DEPTHS, ID_SIZE, c);
 }
 
 criterion_group!(
     benches,
-    bench_lw_new,
     bench_lw_setup,
     bench_lw_keygen,
     bench_lw_encrypt,
